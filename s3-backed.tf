e127e33e02877dcdfbdadb32c82a2b09fe460ad2
terencetatefua <95458889+terencetatefua@users.noreply.github.com>
2025-06-19 15:41:49 -0400

    Update s3-backed.tf

--- s3-backed.tf
+++ s3-backed.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "my-eks-tf-state"
+    bucket = "my-eks-tf-state-admute"
     key    = "eks/terraform.tfstate"
     region = "us-east-2"
     dynamodb_table = "terraform-locks"
