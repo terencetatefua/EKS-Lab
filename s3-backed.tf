f9627b0e40ada14f98bf373cfd161f26dabca04f
terencetatefua <95458889+terencetatefua@users.noreply.github.com>
2025-06-20 09:05:43 -0400

    Update s3-backed.tf

--- s3-backed.tf
+++ s3-backed.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "my-eks-tf-state-admute"
-    key    = "eks/terraform.tfstate"
-    region = "us-east-2"
-    dynamodb_table = "terraform-locks"
+    bucket = "my-eks-tf-state-admute" #Update this your own bucket name
+    key    = "eks/terraform.tfstate"  #Update with your own key
+    region = "us-east-2" #Update to your won region
+    dynamodb_table = "terraform-locks" Update with your own table
   }
 }
