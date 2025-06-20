7e399fd30cf46ec45eb5264b7b0f1cd5467bfbf6
terencetatefua <95458889+terencetatefua@users.noreply.github.com>
2025-06-20 09:04:16 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,9 +1,9 @@
 variable "region" {
   type    = string
-  default = "us-east-2"
+  default = "us-east-2"  # update this to you own region
 }
 
 variable "cluster_name" {
   type    = string
-  default = "my-eks-cluster"
+  default = "tnt-eks-cluster"  #Update this to your own name
 }
