642d418517cc0b9209fdf836230b49dae3f428e2
terencetatefua <95458889+terencetatefua@users.noreply.github.com>
2025-06-24 09:11:08 -0400

    Update s3-backed.tf

--- s3-backed.tf
+++ s3-backed.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "my-eks-tf-state-admute" # Update this the bucket you created earlier
     key    = "eks/terraform.tfstate"  # Update with your own key (Key can be any name you want)
-    region = "us-east-2" # Update to your won region
+    region = "us-east-2" # Update to your own region
     use_lockfile   = true 
   }
 }
