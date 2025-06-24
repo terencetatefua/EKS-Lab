50d2b80317111e278993dc25aa9576e06313c60f
terencetatefua <95458889+terencetatefua@users.noreply.github.com>
2025-06-24 09:09:06 -0400

    Update s3-backed.tf

--- s3-backed.tf
+++ s3-backed.tf
@@ -3,6 +3,6 @@ terraform {
     bucket = "my-eks-tf-state-admute" # Update this the bucket you created earlier
     key    = "eks/terraform.tfstate"  # Update with your own key (Key can be any name you want)
     region = "us-east-2" # Update to your won region
-    dynamodb_table = "terraform-locks" # Update with name of the dynamo db you created earlier
+    use_lockfile   = true 
   }
 }
